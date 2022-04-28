d12feb62d7a44963c752a79afc66a9cb7694e29a
Kevin Devlin <azwethinkweizkd@gmail.com>
2022-04-28 07:12:30 -0600

    removed react aos animation from section titles

--- src/components/section-title/secTitle.component.jsx
+++ src/components/section-title/secTitle.component.jsx
@@ -1,15 +1,10 @@
-import React, { useEffect } from "react";
-import Aos from "aos";
-import "aos/dist/aos.css";
+import React from "react";
 
 import "./secTitle.styles.scss";
 
 export default function SecTitle({ title }) {
-  useEffect(() => {
-    Aos.init({ duration: 250 });
-  }, []);
   return (
-    <div className="section-title" data-aos="fade-down-left">
+    <div className="section-title">
       <h2 className="title">{title}</h2>
     </div>
   );
